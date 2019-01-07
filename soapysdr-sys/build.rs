b19faef658a580cbac8f2097dbfac802f3530282
ywzjackal@163.com <ywzjackal@163.com>
2019-01-07 09:27:25 +0800

    build pass on my ubuntu Signed-off-by: ywzjackal@163.com <ywzjackal@163.com>

--- soapysdr-sys/build.rs
+++ soapysdr-sys/build.rs
@@ -8,10 +8,10 @@ fn main() {
     if let Err(e) = pkg_config::Config::new().atleast_version("0.6.0").probe("SoapySDR") {
         panic!("Couldn't find SoapySDR: {}", e);
     }
-
     let bindings = bindgen::Builder::default()
         .trust_clang_mangling(false)
         .header("wrapper.h")
+        .clang_arg("-I/usr/lib/gcc/x86_64-linux-gnu/7/include")
         .generate()
         .expect("Unable to generate bindings");
 
